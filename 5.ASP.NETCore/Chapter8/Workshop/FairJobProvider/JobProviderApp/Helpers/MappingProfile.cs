1731f35848fd30cd46bf0c839cecf84e17335996
soumyapremrajaitrich <soumyapremraj.aitrich@gmail.com>
2026-05-27 11:37:24 +0530

    add

---
+++ 5.ASP.NETCore/Chapter8/Workshop/FairJobProvider/JobProviderApp/Helpers/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using JobProviderApp.Dto;
+using JobProviderApp.Model;
+namespace JobProviderApp.Helpers
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<JobProvider, JobProviderDto>().ReverseMap();
+            CreateMap<Job, JobDto>().ReverseMap();
+        }
+    }
+}
