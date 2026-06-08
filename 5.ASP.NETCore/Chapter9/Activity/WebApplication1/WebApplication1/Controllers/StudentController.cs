162bda21b88119bc314ba63c3300b52416b3e692
soumyapremrajaitrich <soumyapremraj.aitrich@gmail.com>
2026-06-08 12:49:17 +0530

    add

---
+++ 5.ASP.NETCore/Chapter9/Activity/WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,116 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Mvc;
+
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        
+
+        
+        public static List<Student> _students = new List<Student>
+    {
+        new Student { Id = 1, Name = "Alice", Email = "alice@example.com", Age = 20 },
+        new Student { Id = 2, Name = "Bob", Email = "bob@example.com", Age = 22 }
+    };
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(_students); 
+        }
+
+        public IActionResult Create()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student newStudent)
+        {
+            newStudent.Id = _students.Count + 1;
+            _students.Add(newStudent);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+
+          var student = _students.FirstOrDefault(x => x.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student updatestudent)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(updatestudent);
+            }
+            var student = _students.FirstOrDefault(s => s.Id == updatestudent.Id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            student.Name= updatestudent.Name;
+            student.Email= updatestudent.Email;
+            student.Age= updatestudent.Age;
+
+            return RedirectToAction("Index");
+
+
+
+
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var student=_students.FirstOrDefault(s =>s.Id == id);
+            if (student == null)
+            {
+
+                return NotFound();
+            }
+            return View(student);
+
+
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+
+
+        public IActionResult DeleteConfirmed(int id)
+        {
+
+
+            var student=_students.FirstOrDefault(s =>s.Id == id);
+            if(student == null)
+            {  return NotFound();
+            }
+
+            _students.Remove(student);
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        
+        
+        
+        
+        
+   }
+}
---
+++ 5.ASP.NETCore/Chapter9/Activity/WebApplication1/WebApplication1/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+
+
+    }
+}
